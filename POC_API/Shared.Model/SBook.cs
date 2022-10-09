49c36d676531e9abb9b63a09373d7aaea67e7d53
unknown <cogdotnet1696@iiht.tech>
2022-10-09 20:20:40 +0530

    rabbitmq initial setup

---
+++ POC_API/Shared.Model/SBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shared.Model
+{
+    public class SBook
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int? AuthorId { get; set; }
+        public string Publisher { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+        public string ActiveStatus { get; set; }
+        public bool? Blocked { get; set; }
+        public int? Price { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
